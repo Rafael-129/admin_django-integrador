5dc46e1caee7e58dfbacd7bac6e48a79ace039dc
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-07-07 02:58:44 -0500

    Bug Arreglado, dashboard 2.0

--- admin_project/admin_project/urls.py
+++ admin_project/admin_project/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from agricultura.views import api_root, AdminUserCreateView, MyTokenObtainPairView
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView
 from agricultura.views import (
     UsuariosViewSet,
     CultivoViewSet,
     TiposTerrenoViewSet,
     RecomendacionesViewSet,
+    AdminUserCreateView,
 )
 
 router = routers.DefaultRouter()
@@ -17,6 +18,8 @@ router.register(r'recomendaciones', RecomendacionesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/create-admin/', AdminUserCreateView.as_view(), name='create_admin'),
     path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/', api_root, name='api-root'),
 ]
--- admin_project/urls.py
+++ admin_project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from agricultura.views import api_root, AdminUserCreateView
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 from agricultura.views import (
@@ -17,7 +18,9 @@ router.register(r'recomendaciones', RecomendacionesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/create-admin/', AdminUserCreateView.as_view(), name='create_admin'),
+    path('api/', include(router.urls)),  # Esto debe ir antes que api_root
+    path('api/', api_root, name='api-root'),  # Esto SOLO debe ser para /api/
 ]
 
