0a9d72b5c575fe274793d066dece23f119920cac
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-07-07 05:32:36 -0500

    Cambiada la URL de la API en AdminLogin para utilizar el nuevo dominio en producción.

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -13,7 +13,7 @@ function AdminLogin() {
     e.preventDefault();
     setError("");
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/token/", {
+      const response = await fetch("https://administrador-agrotech-bakcend.onrender.com/api/token/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
