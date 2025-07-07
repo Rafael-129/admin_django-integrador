ec5d56c4a92078e07c6656022b174abed9128fc8
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-07-07 03:16:03 -0500

    bug arreglado

--- admin_project/urls.py
+++ admin_project/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from agricultura.views import api_root, AdminUserCreateView
+from agricultura.views import api_root, AdminUserCreateView, MyTokenObtainPairView
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView
 from agricultura.views import (
     UsuariosViewSet,
     CultivoViewSet,
@@ -18,7 +17,7 @@ router.register(r'recomendaciones', RecomendacionesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/create-admin/', AdminUserCreateView.as_view(), name='create_admin'),
     path('api/', include(router.urls)),  # Esto debe ir antes que api_root
     path('api/', api_root, name='api-root'),  # Esto SOLO debe ser para /api/
